9ef3fc7058cbd431c2ac04222088d81bbc33c452
Yurim <152935798+zyu22@users.noreply.github.com>
2024-09-13 22:12:58 +0900

    [BOJ] N과M12 / 실버2 / 5분
    https://www.acmicpc.net/problem/15666

---
+++ baekjoon/15666.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main15666_N과M12 {
+	static int N, M;
+	static int[] nums;
+	static int[] p;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		
+		p = new int[N];
+		nums = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+	
+		Arrays.sort(p);
+		perm(0, 0);
+		
+		System.out.println(sb);
+	}
+	
+	static void perm(int cnt, int start) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(nums[i]).append(" ");
+			}
+			
+			sb.append("\n");
+			
+			return;
+		}
+		
+		
+		int prev = 0;
+		for(int i = start; i < N; i++) {
+			
+			if(prev != p[i]) {
+				nums[cnt] = p[i];
+				prev = p[i];
+				perm(cnt+1, i);
+			}
+		}
+	}
+}
